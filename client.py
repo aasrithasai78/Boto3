2c2413df14836c94cb7ccd5b6050dcc68aae2468
Aasritha Sai Vemulapalli <98679138+aasrithasai78@users.noreply.github.com>
2024-04-18 10:29:55 +0530

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,16 @@ from pprint import pprint
 aws_console = boto3.session.Session(profile_name="default")
 iam_console = aws_console.client(service_name="iam")
 user=iam_console.list_users()
-pprint(user)
+pprint(user['Users'])
+
+
+#Print only the users 
+import boto3
+from pprint import pprint
+aws_console = boto3.session.Session(profile_name="default")
+iam_console = aws_console.client(service_name="iam")
+for user in iam_console.list_users()['Users']:
+  print(user['UserName'])
 
 
 
